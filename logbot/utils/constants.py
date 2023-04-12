e7f2ea3ad7c6373da6d3f5c24310967dc53b9ea7
talha-zshan <talhazshan@gmail.com>
2023-04-13 02:46:40 +0300

    Add Mason Builder and Constants

---
+++ logbot/utils/constants.py
@@ -0,0 +1,12 @@
+"""
+This module contains constants used by our API
+"""
+
+MASON = "application/vnd.mason+json"
+
+# USER_AUTHENTICATION = "/authentication/student/"
+# COURSE_PROFILE = "/profiles/course/"
+# ASSESSMENT_PROFILE = "/profiles/assessment/"
+ERROR_PROFILE = "/logbot/error/"
+
+LINK_RELATIONS_URL = "/logbot/link-relations/"
